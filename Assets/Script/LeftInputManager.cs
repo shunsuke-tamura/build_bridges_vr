0f019c18a604794e8f3ac83762b4f64e93879892
shunsuke-tamura <wwwxx4869@gmail.com>
2022-05-06 09:37:05 +0900

    update: 橋の伸縮をピボットでできるようにした

--- Assets/Script/LeftInputManager.cs
+++ Assets/Script/LeftInputManager.cs
@@ -46,7 +46,8 @@ public class LeftInputManager : MonoBehaviour
 
                 if (OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
                 {
-                    hit.transform.localScale = new Vector3(hit.transform.localScale.x, hit.transform.localScale.y - 0.1f, hit.transform.localScale.z);
+                    GameObject parent = hit.transform.parent.gameObject;
+                    parent.transform.localScale = new Vector3(parent.transform.localScale.x, parent.transform.localScale.y - 0.1f, parent.transform.localScale.z);
                 }
                 break;
             }
